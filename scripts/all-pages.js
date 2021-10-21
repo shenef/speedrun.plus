1881eca210e52a63793ff792b274cc9f05112b97
YummyBacon5 <68166338+YummyBacon5@users.noreply.github.com>
2021-10-21 03:32:58 +0100

    More code, closed issue #3
    The followedIcons checkbox should now work, it is slow lol I'll fix that later
    turnOffAds doesn't do anything yet
    I have updated the Extension Settings icon and panel
    issue #3 has been fixed

--- scripts/all-pages.js
+++ scripts/all-pages.js
@@ -1,12 +1,24 @@
 //This adds the Extension Settings button
-document.getElementsByClassName("fal fa-cog fa-margin")[0].parentNode.outerHTML += `<a class="dropdown-item" href="#SRPsettings" data-toggle="modal"><i class="fal fa-cog fa-margin"></i>Extension Settings</a>`
+//This adds it in the username
+const settingsIcon = document.getElementsByClassName("fal fa-cog fa-margin")[0];
+const signedIn = JSON.parse(document.querySelector("head [name='src:session']").getAttribute("content")).signedIn
+chrome.storage.sync.get(["extensionIconInNav"], function(result) {
+	if (!signedIn || result.extensionIconInNav == "1") {
+		//This adds it in the nav bar, it will show here for logged out users
+		document.getElementsByClassName("navbar-nav")[1].insertAdjacentHTML("afterbegin", `<li class="nav-item dropdown"><a class="nav-link" href="#SRPsettings" data-toggle="modal"><i class="fal fa-cog fa-margin"></i><span class="badge badge-counter"></span></a></li>`);
+		return;
+	}
+	if (signedIn) {
+		settingsIcon.parentNode.outerHTML += `<a class="dropdown-item" href="#SRPsettings" data-toggle="modal"><i class="fal fa-cog fa-margin"></i>Extension Settings</a>`
+	}
+})
 
 //This adds the settings menu
 document.getElementsByClassName("navbar-background fixed-top")[0].outerHTML += `<div class="modal fade formatting-help" id="SRPsettings" role="dialog" style="display: none;" aria-hidden="true">
 	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title">Extension Settings</h5>
+				<h5 class="modal-title"><img src="https://cdn.discordapp.com/icons/899603056235282442/343f7924b151f4975f4478f49d78dabc.webp" class="fal fa-info-circle fa-margin"></img>Extension Settings</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">x</span>
 				</button>
@@ -21,36 +33,52 @@ document.getElementsByClassName("navbar-background fixed-top")[0].outerHTML += `
 				<div class="tab-content">
 					<div role="tabpanel" class="tab-pane" id="gameRules">inner text</div>
 					<div role="tabpanel" class="tab-pane active show" id="categoryRules">
-						<label class="switch"><input id="turnOffAds" type="checkbox" class="SRP checkbox"><span class="slider"></span></label><label for="turnOffAds">Turn off ads</label><br>
-						<label class="switch"><input id="test" type="checkbox" class="SRP checkbox"><span class="slider"></span></label><label for="test">Test</label>
-						</div>
+						Reload the page to apply changes<br>
+						<label class="switch"><input id="turnOffAds" type="checkbox" class="SRPcheckbox"><span class="slider"></span></label><label for="turnOffAds">Turn off ads</label><br>
+						<label class="switch"><input id="followedIcons" type="checkbox" class="SRPcheckbox"><span class="slider"></span></label><label for="followedIcons">Show game's cover in Games dropdown</label><br>
+						<label class="switch"><input id="extensionIconInNav" type="checkbox" class="SRPcheckbox"><span class="slider"></span></label><label for="extensionIconInNav">Show the <i class="fal fa-cog fa-margin"></i> extension settings icon in the nav bar</label><br>
+  					</div>
 				</div>
 			</div>
 			<div class="modal-footer">
 				<a class="btn btn-default" data-dismiss="modal">Close</a>
+				<a id="SRPsave" class="btn btn-primary">Save</a>
 			</div>
 		</div>
 	</div>
 </div>`
 
-const checkbox = document.getElementsByClassName("SRP checkbox");
+const checkbox = document.getElementsByClassName("SRPcheckbox");
 for (i = 0; i < checkbox.length; i++) {
 	let theId = checkbox[i].id;
 
-	chrome.storage.sync.get([theId], function (result) {
+	chrome.storage.sync.get([theId], function(result) {
 		if (result[theId] == "1") {
 			document.getElementById(theId).checked = true;
 		}
 	});
 
-	checkbox[i].addEventListener("change", function () {
-		chrome.storage.sync.set({[theId]: this.checked ? "1" : "0" });
+	checkbox[i].addEventListener("change", function() {
+		chrome.storage.sync.set({
+			[theId]: this.checked ? "1" : "0"
+		});
 	})
 }
 
-// Add game covers next to the followed games in the game list
-const followedGames = document.getElementsByClassName("dropdown-menu")[0].children;
-for (i = 3; i < followedGames.length; i++) {
-	let res = await fetch(`https://www.speedrun.com/api/v1/games/${followedGames[i].href.split("/")[3]}`).then(res => res.json());
-	followedGames[i].innerHTML = `<img class="fal fa-info-circle fa-margin" src="${res.data.assets["cover-large"].uri}"></img>` + followedGames[i].innerHTML
-}
+chrome.storage.sync.get(["followedIcons"], async function(result) {
+	if (signedIn && result.followedIcons == "1") {
+		//Add game covers next to the followed games in the game list
+		const followedGames = document.getElementsByClassName("dropdown-menu")[0].children;
+		for (i = 3; i < followedGames.length; i++) {
+			let res = await fetch(`https://www.speedrun.com/api/v1/games/${followedGames[i].href.split("/")[3]}`).then(res => res.json());
+			followedGames[i].innerHTML = `<img class="fal fa-info-circle fa-margin" src="${res.data.assets["cover-large"].uri}"></img>` + followedGames[i].innerHTML
+		}
+	}
+})
+
+chrome.storage.sync.get(["turnOffAds"], function(result) {
+	if (result.turnOffAds == "1") {
+		//The code needs to be added
+		console.log("The code needs to be added")
+	}
+})
