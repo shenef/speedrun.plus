bbe22eff3ccfc4ca21326ba29e5662eb061bfdeb
YummyBacon5 <68166338+YummyBacon5@users.noreply.github.com>
2021-10-21 02:20:46 +0100

    Removed and reset my API key

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,10 +2,10 @@ const res = await fetch("https://www.speedrun.com/api/v1/notifications", {
 	method: "GET",
 	mode: 'cors',
 	headers: {
-		"X-API-Key": "zo0bh3o9omd8wkkbugucu9n78",
+		"X-API-Key": "",
 	},
 }).then(res => res.json())
 
 for (i = 0; i < res.data.length; i++) {
-	document.getElementById("SRCnotifications").innerHTML += `<a class="dropdown-item border-top pt-2 pb-2" href="${res.data[i].item.uri}">${res.data[i].text}<small class="text-muted">· <time class="short" datetime="${res.data[i].created}" title="${new Date(res.data[i].created)}">time agoo</time></small></a>`
+	document.getElementById("SRCnotifications").innerHTML += `<a class="dropdown-item border-top pt-2 pb-2" href="${res.data[i].item.uri}">${res.data[i].text}<small class="text-muted">Â· <time class="short" datetime="${res.data[i].created}" title="${new Date(res.data[i].created)}">time agoo</time></small></a>`
 }
